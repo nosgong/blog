a933280d9c0c294bb77a29a35e1023e12bbd8cf1
nosgong <nosgong@163.com>
2019-10-18 09:44:07 +0800

    project init

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::any('{module}/{class}/{action}', function ($module, $class, $action) {
+//    $ctrl = \App::make("\\App\\Http\\Controllers\\" . $module . "\\" . $class . "Controller");
+//    return \App::call([$ctrl, $action]);
+//});
+
+//Route::get('test/{method}', 'API\TestApiController@method');
+Route::any('test', 'API\TestApiController@testUrl');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
